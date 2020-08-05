7efc1d01edc66b8fcc0d7f350cb2361e2c23e5e2
github username <rdg.brg@gmail.com>
2020-08-05 13:29:12 -0400

    Add abstraction to useState - hooks folder

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useToggle from './hooks/useToggle'
 
 const Toggle = () => {
 
-  const [ isHappy, setIsHappy ] = useState(true)
-  const [ isInLove, setIsInLove ] = useState(true)
-
-  const toggleIsHappy = () => {
-    setIsHappy(!isHappy)
-  }
-
-  const toggleIsInLove = () => {
-    setIsInLove(!isInLove) 
-  }
+  const [ isHappy, toggleIsHappy ] = useToggle(true)
+  const [ isInLove, toggleIsInLove ] = useToggle(true)
 
   return(
     <div>
@@ -19,12 +12,14 @@ const Toggle = () => {
         onClick={toggleIsHappy} 
         style={{height: "100px", width: "100px", margin: "20px 40px"}} 
         src={isHappy ? require('./emojis/happy.png') : require('./emojis/sad.png')} 
+        alt={isHappy ? "happy emoji" : "sad emoji" }
         />
 
       <img 
         onClick={toggleIsInLove}
         style={{height: "100px", width: "100px", margin: "20px auto"}} 
-        src={isInLove ? require('./emojis/smile.png') : require('./emojis/inlove.png')} />
+        src={isInLove ? require('./emojis/smile.png') : require('./emojis/inlove.png')}
+        alt={isInLove ? "smile emoji" : "inlove emoji" } />
     </div>
   )
 }
